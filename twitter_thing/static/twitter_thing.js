4ec62efad0ce744d03271dd97cddd4632513bb6e
Gregory Bell <gregoryb@ualberta.net>
2012-07-11 20:33:40 -0600

    Am now getting results back from Twitter.
    That was dumb. The reason I wasn't getting any results back from Twitter was that my query string didn't have the params separated properly. So I had this: '?q=puppies?callback=?', instead of '?q=puppies&callback=?'. I'm not actually sure how that extra question mark works. I'm gussing jQuery replaces it with a proper callback name that it invents itself.

--- twitter_thing/static/twitter_thing.js
+++ twitter_thing/static/twitter_thing.js
@@ -1,6 +1,6 @@
 (function($) {
 	// https://twitter.com/#!/search/puppies%20OR%20kitties%20filter%3Aimages
-	$.getJSON('http://search.twitter.com/search.json?q=puppies?callback=?', function(data, textStatus, jqXHR){
+	$.getJSON('http://search.twitter.com/search.json?q=puppies&callback=?', function(data, textStatus, jqXHR){
 		debugger;
 	});
 })(jQuery);
