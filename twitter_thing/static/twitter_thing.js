e165cf8d7d7f27a380f1e5c644df2f164ad16cba
Gregory Bell <gregoryb@ualberta.net>
2012-07-12 15:14:52 -0600

    It does stuff! Shows Instagram images from tweets about puppies and kitties.
    Now that it does something time to clean it up and make it look nice. I can add more filter like twitpic and lockerz later.
    
    Vague ToDo:
    Single column
    Some kind of border/separation
    Timer so the don't all flood in at once
    Repeat query so it keeps getting more images.

--- twitter_thing/static/twitter_thing.js
+++ twitter_thing/static/twitter_thing.js
@@ -1,9 +1,12 @@
 (function($) {
 	// https://twitter.com/#!/search/puppies%20OR%20kitties%20filter%3Aimages
-	$.getJSON('http://search.twitter.com/search.json?q=puppy%20OR%20puppies%20OR%20kitty%20OR%20kitties%20filter%3Aimages&include_entities=true&rpp=100&callback=?', function(data, textStatus, jqXHR){
+	$.getJSON('http://search.twitter.com/search.json?q=puppy%20OR%20puppies%20OR%20kitty%20OR%20kitties%20filter%3Aimages&include_entities=true&rpp=25&callback=?', function(data, textStatus, jqXHR){
 		data.results.forEach(function(value, index, array){
-			if( typeof value.entities.media != "undefined" ){
-				debugger;
+			if( typeof value.entities.urls != "undefined" ){
+				if (value.entities.urls[0].display_url.search(/instagr/) != -1) {
+					var src = value.entities.urls[0].expanded_url + "/media/?size=l"
+					jQuery("#images").append('<img src="' + src + '" />')
+				}
 			}
 		});
 		debugger;
